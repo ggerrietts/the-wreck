f18f893c6ece84bce6c61d3080af0949a3c1be28
Geoff Gerrietts <ggerrietts@appneta.com>
2016-05-13 14:18:29 -0400

    better logging from flask

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,5 +1,6 @@
 from app import app
 from oboeware import OboeMiddleware
+import logging
 
 app.wsgi_app = OboeMiddleware(app.wsgi_app)
 
@@ -9,6 +10,11 @@ import noisy_neighbor
 # import memory_grenade
 
 application = app
+
+# Log errors.
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+app.logger.addHandler(stream_handler)
 
 if __name__ == "__main__":
     app.run()
