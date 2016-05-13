5c10fe182d9111206d14c1bc5ddb3fb7379c95d6
Geoff Gerrietts <ggerrietts@appneta.com>
2016-05-13 14:22:47 -0400

    add latency logging to nginx, turn up gunicorn workers

--- chef/cookbooks/tw-webserver/recipes/nginx.rb
+++ chef/cookbooks/tw-webserver/recipes/nginx.rb
@@ -4,14 +4,28 @@
 
 package "nginx-full"
 
+service 'nginx' do
+    supports :restart => true, :start => true, :stop => true
+    action :nothing
+end
+
 template "/etc/nginx/sites-available/thewreck" do
   source "thewreck.erb"
+  notifies :restart, 'service[nginx]', :delayed
 end
 
 link "/etc/nginx/sites-enabled/thewreck" do
   to "/etc/nginx/sites-available/thewreck"
+  notifies :restart, 'service[nginx]', :delayed
 end
 
-service "nginx" do
-  action :restart
+file "/etc/nginx/sites-enabled/default" do
+  action :delete
+  notifies :restart, 'service[nginx]', :delayed
 end
+
+cookbook_file "/etc/nginx/nginx.conf" do
+  source "nginx.conf"
+  notifies :restart, 'service[nginx]', :delayed
+end
+
